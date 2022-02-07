bcd7f52c3449f52b743d8151e87b47474b27e586
hernangit config --global user.name John <hernan0066@gmail.com>
2022-02-07 19:01:30 -0300

    Register-Screen

--- src/screens/Account/Register.js
+++ src/screens/Account/Register.js
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { StyleSheet, View, Image, Text } from "react-native";
+import RegisterForm from '../../components/Account/RegisterForm';
+//import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+//import Toast from "react-native-easy-toast";
+//import RegisterForm from "../../components/Account/RegisterForm";
 
-const Register = () => {
+export default function Register() {
+  
+
   return (
     <View>
-      <Text>Register</Text>
+       <Image 
+     source={require('../../../assets/img/logo.png')} 
+     resizeMode='contain'
+     style={styles.logo}
+     />
+     <View style={styles.viewForm}>
+       <RegisterForm />
+      
+     </View>
     </View>
   );
-};
+}
 
-export default Register;
+const styles = StyleSheet.create({
+  logo: {
+    width: "100%",
+    height: 150,
+    marginTop: 20,
+  },
+  viewForm: {
+    marginRight: 40,
+    marginLeft: 40,
+  },
+});
